54e180d156eb5556322466fc18fd4028a4b9a44a
conurfloki <conurfloki@gmail.com>
2023-10-20 19:44:27 -0300

    add - any order of curves

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"math"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
-
 	width, heigth := int32(512), int32(512)
-	order := 1
+	order := 4
 	n := int32(math.Pow(2, float64(order)))
 	total := n * n
 	path := []rl.Vector2{}
+
 	for i := 0; i < int(total); i++ {
-		path = append(path, hilbert(i))
+		path = append(path, hilbert(i, order))
 		len := float32(width / n)
 		path[i].X = path[i].X * len
 		path[i].Y = path[i].Y * len
@@ -41,7 +42,7 @@ func main() {
 	rl.CloseWindow()
 }
 
-func hilbert(i int) rl.Vector2 {
+func hilbert(i, order int) rl.Vector2 {
 	vectorArr := []rl.Vector2{}
 
 	vectorArr = append(vectorArr, rl.Vector2{X: 0, Y: 0})
@@ -49,5 +50,30 @@ func hilbert(i int) rl.Vector2 {
 	vectorArr = append(vectorArr, rl.Vector2{X: 1, Y: 1})
 	vectorArr = append(vectorArr, rl.Vector2{X: 1, Y: 0})
 
-	return vectorArr[i]
+	index := i & 3
+	v := vectorArr[index]
+	fmt.Println("estamo activo?")
+	for j := 1; j < order; j++ {
+		fmt.Println("estamo activo loop?")
+		i = i >> 2
+		index = i & 3
+		len := math.Pow(2, float64(j))
+		switch index {
+		case 0:
+			temp := v.X
+			v.X = v.Y
+			v.Y = temp
+		case 1:
+			v.Y += float32(len)
+		case 2:
+			v.X += float32(len)
+			v.Y += float32(len)
+		case 3:
+			temp := float32(len) - 1 - v.X
+			v.X = float32(len) - 1 - v.Y
+			v.Y = temp
+			v.X += float32(len)
+		}
+	}
+	return v
 }
